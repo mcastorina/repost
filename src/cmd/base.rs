6b90c15e322e77eeac951ab50ea57d30fb0f45c5
Miccah Castorina <m.castorina93@gmail.com>
2020-06-22 22:34:33 -0500

    Move run command to BaseCommand and make it work without args
    When a user has a request set, doing "run" will run the request that
    they are currently in.

--- src/cmd/base.rs
+++ src/cmd/base.rs
@@ -1,8 +1,10 @@
 use crate::cmd::{Cmd, CmdError};
-use crate::db::{Method, PrintableTable, Request, Variable};
+use crate::db::{Method, PrintableTable, Request, RequestOutput, Variable};
 use crate::Repl;
 use clap_v3::{App, AppSettings, Arg, ArgMatches};
+use colored::*;
 use comfy_table::{ContentArrangement, Table, TableComponent};
+use reqwest::blocking;
 use std::fs;
 use terminal_size::{terminal_size, Width};
 
@@ -44,6 +46,7 @@ impl Cmd for BaseCommand {
                 ("variables", Some(matches)) => BaseCommand::delete_variables(repl, matches),
                 _ => unreachable!(),
             },
+            ("run", Some(matches)) => BaseCommand::execute_run(repl, matches),
             _ => unreachable!(),
         }
     }
@@ -167,8 +170,141 @@ impl BaseCommand {
         println!("\n{}\n", table);
         Ok(())
     }
+    fn execute_run(repl: &mut Repl, matches: &ArgMatches) -> Result<(), CmdError> {
+        let req = matches.value_of("request").unwrap();
+        BaseCommand::run(repl, matches, req)
+    }
+    pub fn run(repl: &mut Repl, matches: &ArgMatches, req: &str) -> Result<(), CmdError> {
+        let mut req = repl.db.get_request(req)?;
+        // get options for this request
+        let input_opts = repl
+            .db
+            .get_input_options()?
+            .into_iter()
+            .filter(|x| req.name() == x.request_name())
+            .collect();
+        let output_opts: Vec<RequestOutput> = repl
+            .db
+            .get_output_options()?
+            .into_iter()
+            .filter(|x| req.name() == x.request_name())
+            .collect();
+        // do option substitution
+        // TODO: return result with missing options
+        if !req.substitute_options(&input_opts) {
+            return Err(CmdError::MissingOptions);
+        }
+
+        let reqw = create_reqwest(&mut req)?;
+        let quiet = matches.is_present("quiet");
+
+        if !quiet {
+            println!(
+                "{}",
+                format!("> {} {}", reqw.method(), reqw.url()).bright_black()
+            );
+            for header in reqw.headers() {
+                let (name, value) = header;
+                println!(
+                    "{}",
+                    format!("> {}: {}", name, value.to_str().unwrap()).bright_black()
+                );
+            }
+            println!();
+        }
+
+        let mut resp = blocking::Client::new().execute(reqw)?;
+
+        // output response code and headers
+        if !quiet {
+            println!("{}", format!("< {}", resp.status()).bright_black());
+            for header in resp.headers() {
+                let (name, value) = header;
+                println!(
+                    "{}",
+                    format!("< {}: {}", name, value.to_str().unwrap()).bright_black()
+                );
+            }
+            println!();
+        }
+
+        // output body with missing-newline indicator
+        let mut text: Vec<u8> = vec![];
+        resp.copy_to(&mut text)?;
+        let text = String::from_utf8(text).unwrap();
+
+        // TODO: invoke $PAGER if length > $LINES
+        // (note: $LINES and $COLUMNS are not exported by default)
+        //       pretty print JSON
+
+        let v: Result<serde_json::Value, serde_json::Error> = serde_json::from_str(&text);
+        if v.is_ok() {
+            let v = v.unwrap();
+            println!("{}", serde_json::to_string_pretty(&v).unwrap());
+        } else {
+            print!("{}", text);
+            if !(&text).ends_with('\n') {
+                println!("{}", "%".bold().reversed());
+            }
+        }
+        if output_opts.len() > 0 {
+            println!();
+        }
+
+        // extract options into variables
+        for opt in output_opts {
+            let mut var = match opt.extraction_source() {
+                "body" => repl.body_to_var(&opt, &text)?,
+                "header" => repl.hader_to_var(&opt, resp.headers())?,
+                x => {
+                    println!("Encountered unexpected source: {}", x);
+                    continue;
+                }
+            };
+            var.source = Some(String::from(req.name()));
+            if !quiet {
+                println!(
+                    "{}",
+                    format!("{} => {}", var.name(), var.value().unwrap_or("")).bright_black()
+                );
+            }
+            repl.db.upsert_variable(var)?;
+            repl.update_options_for_variable(opt.option_name())?;
+        }
+
+        Ok(())
+    }
 }
 
+fn create_reqwest(req: &mut Request) -> Result<blocking::Request, CmdError> {
+    // TODO: should this be a method of Request?
+    let client = blocking::Client::new();
+    let mut builder = match req.method() {
+        Method::GET => client.get(req.url()),
+        Method::POST => client.post(req.url()),
+        Method::PUT => client.put(req.url()),
+        Method::PATCH => client.patch(req.url()),
+        Method::DELETE => client.delete(req.url()),
+        Method::HEAD => client.head(req.url()),
+    };
+    // add headers
+    if let Some(x) = req.headers() {
+        for hv in x.split('\n') {
+            let mut items = hv.splitn(2, ":");
+            let (header, value) = (items.next(), items.next());
+            if header.and(value).is_none() {
+                continue;
+            }
+            builder = builder.header(header.unwrap(), value.unwrap());
+        }
+    }
+    // add body
+    if let Some(x) = req.consume_body() {
+        builder = builder.body(x);
+    }
+
+    Ok(builder.build()?)
+}
 fn clap_args() -> clap_v3::App<'static> {
     // TODO: can this be a sinlge static clap_v3::App variable?
     let contains_equal = |val: String| {
@@ -334,5 +470,24 @@ fn clap_args() -> clap_v3::App<'static> {
                 .aliases(&["remove", "del", "rm"])
                 .subcommand(delete_requests)
                 .subcommand(delete_variables),
+        )
+        .subcommand(
+            App::new("run")
+                .about("Run a named HTTP request")
+                .aliases(&["r"])
+                .arg(
+                    Arg::with_name("request")
+                        .help("Request to run")
+                        .required(true)
+                        .multiple(false), // TODO run multiple in a row
+                )
+                .arg(
+                    Arg::with_name("quiet")
+                        .help("Verbose output")
+                        .short('q')
+                        .long("quiet")
+                        .takes_value(false)
+                        .required(false),
+                ),
         )
 }
--- src/cmd/environmental.rs
+++ src/cmd/environmental.rs
@@ -1,9 +1,7 @@
-use crate::cmd::{Cmd, CmdError};
-use crate::db::{Method, Request, RequestOutput};
+use crate::cmd::{BaseCommand, Cmd, CmdError};
+use crate::db::{RequestOutput};
 use crate::Repl;
 use clap_v3::{App, AppSettings, Arg, ArgMatches};
-use colored::*;
-use reqwest::blocking;
 
 pub struct ContextualCommand {}
 impl Cmd for ContextualCommand {
@@ -18,105 +16,11 @@ impl Cmd for ContextualCommand {
 }
 impl ContextualCommand {
     fn execute_run(repl: &mut Repl, matches: &ArgMatches) -> Result<(), CmdError> {
-        let req = matches.value_of("request").unwrap();
-        let mut req = repl.db.get_request(req)?;
-        // get options for this request
-        let input_opts = repl
-            .db
-            .get_input_options()?
-            .into_iter()
-            .filter(|x| req.name() == x.request_name())
-            .collect();
-        let output_opts: Vec<RequestOutput> = repl
-            .db
-            .get_output_options()?
-            .into_iter()
-            .filter(|x| req.name() == x.request_name())
-            .collect();
-        // do option substitution
-        // TODO: return result with missing options
-        if !req.substitute_options(&input_opts) {
-            return Err(CmdError::MissingOptions);
+        if repl.request().is_none() {
+            return Err(CmdError::NotFound);
         }
-
-        let reqw = create_reqwest(&mut req)?;
-        let quiet = matches.is_present("quiet");
-
-        if !quiet {
-            println!(
-                "{}",
-                format!("> {} {}", reqw.method(), reqw.url()).bright_black()
-            );
-            for header in reqw.headers() {
-                let (name, value) = header;
-                println!(
-                    "{}",
-                    format!("> {}: {}", name, value.to_str().unwrap()).bright_black()
-                );
-            }
-            println!();
-        }
-
-        let mut resp = blocking::Client::new().execute(reqw)?;
-
-        // output response code and headers
-        if !quiet {
-            println!("{}", format!("< {}", resp.status()).bright_black());
-            for header in resp.headers() {
-                let (name, value) = header;
-                println!(
-                    "{}",
-                    format!("< {}: {}", name, value.to_str().unwrap()).bright_black()
-                );
-            }
-            println!();
-        }
-
-        // output body with missing-newline indicator
-        let mut text: Vec<u8> = vec![];
-        resp.copy_to(&mut text)?;
-        let text = String::from_utf8(text).unwrap();
-
-        // TODO: invoke $PAGER if length > $LINES
-        // (note: $LINES and $COLUMNS are not exported by default)
-        //       pretty print JSON
-
-        let v: Result<serde_json::Value, serde_json::Error> = serde_json::from_str(&text);
-        if v.is_ok() {
-            let v = v.unwrap();
-            println!("{}", serde_json::to_string_pretty(&v).unwrap());
-        } else {
-            print!("{}", text);
-            if !(&text).ends_with('\n') {
-                println!("{}", "%".bold().reversed());
-            }
-        }
-        if output_opts.len() > 0 {
-            println!();
-        }
-
-        // extract options into variables
-        for opt in output_opts {
-            let mut var = match opt.extraction_source() {
-                "body" => repl.body_to_var(&opt, &text)?,
-                "header" => repl.hader_to_var(&opt, resp.headers())?,
-                x => {
-                    println!("Encountered unexpected source: {}", x);
-                    continue;
-                }
-            };
-            var.source = Some(String::from(req.name()));
-            if !quiet {
-                println!(
-                    "{}",
-                    format!("{} => {}", var.name(), var.value().unwrap_or("")).bright_black()
-                );
-            }
-            repl.db.upsert_variable(var)?;
-            repl.update_options_for_variable(opt.option_name())?;
-        }
-
-        Ok(())
+        let req = String::from(repl.request().unwrap());
+        BaseCommand::run(repl, matches, req.as_ref())
     }
 
     fn extract(repl: &mut Repl, matches: &ArgMatches) -> Result<(), CmdError> {
@@ -134,35 +38,6 @@ impl ContextualCommand {
     }
 }
 
-fn create_reqwest(req: &mut Request) -> Result<blocking::Request, CmdError> {
-    // TODO: should this be a method of Request?
-    let client = blocking::Client::new();
-    let mut builder = match req.method() {
-        Method::GET => client.get(req.url()),
-        Method::POST => client.post(req.url()),
-        Method::PUT => client.put(req.url()),
-        Method::PATCH => client.patch(req.url()),
-        Method::DELETE => client.delete(req.url()),
-        Method::HEAD => client.head(req.url()),
-    };
-    // add headers
-    if let Some(x) = req.headers() {
-        for hv in x.split('\n') {
-            let mut items = hv.splitn(2, ":");
-            let (header, value) = (items.next(), items.next());
-            if header.and(value).is_none() {
-                continue;
-            }
-            builder = builder.header(header.unwrap(), value.unwrap());
-        }
-    }
-    // add body
-    if let Some(x) = req.consume_body() {
-        builder = builder.body(x);
-    }
-
-    Ok(builder.build()?)
-}
 fn clap_args() -> clap_v3::App<'static> {
     // TODO: can this be a sinlge static clap_v3::App variable?
     App::new("repost")
@@ -173,12 +48,13 @@ fn clap_args() -> clap_v3::App<'static> {
             App::new("run")
                 .about("Run a named HTTP request")
                 .aliases(&["r"])
-                .arg(
-                    Arg::with_name("request")
-                        .help("Request to run")
-                        .required(true)
-                        .multiple(false), // TODO run multiple in a row
-                )
+                // TODO
+                // .arg(
+                //     Arg::with_name("request")
+                //         .help("Request to run")
+                //         .required(false)
+                //         .multiple(false), // TODO run multiple in a row
+                // )
                 .arg(
                     Arg::with_name("quiet")
                         .help("Verbose output")
